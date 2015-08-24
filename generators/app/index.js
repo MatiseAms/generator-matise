82b6fa4924a1158a36f155744619d8ff2ed0b5f8
Stephan Hoogland <stephan@shoogland.com>
2015-08-24 16:59:19 +0200

    changed some code

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,10 +4,10 @@ module.exports = generators.Base.extend({
   // The name `constructor` is important here
   constructor: function () {
     // Calling the super constructor is important so our generator is correctly set up
-    generators.Base.apply(this, arguments);  
+    generators.Base.apply(this, arguments);
   },
   method1: function () {
-    console.log('method 1 just ran');
+    console.log('my first yo generator!!');
   },
   method2: function () {
     console.log('method 2 just ran');
