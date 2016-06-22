7b251f62214e6909e79c7cf9720f5eb2d005a2c4
Stephan Hoogland <stephan@shoogland.com>
2016-06-22 13:36:02 +0200

    fixed sils scss dingen

--- app/index.js
+++ app/index.js
@@ -125,161 +125,20 @@ module.exports = generators.Base.extend({
 		if (answers.projectType === 'wordpress') {
 			scssDestination = 'themesrc/';
 		}
+		// Mixins folder
 		this.fs.copy(
-			this.templatePath('scss/mixins/_animation.scss'),
-			this.destinationPath(scssDestination + 'scss/_animation.scss')
+			this.templatePath('scss/mixins/*'),
+			this.destinationPath(scssDestination + 'scss/mixins')
 		);
+		// Zurb folder
 		this.fs.copy(
-			this.templatePath('scss/mixins/_background.scss'),
-			this.destinationPath(scssDestination + 'scss/_background.scss')
+			this.templatePath('scss/zurb/*'),
+			this.destinationPath(scssDestination + 'scss/zurb')
 		);
+		//global sccss
 		this.fs.copy(
-			this.templatePath('scss/mixins/_breakpoints.scss'),
-			this.destinationPath(scssDestination + 'scss/_breakpoints.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_elements.scss'),
-			this.destinationPath(scssDestination + 'scss/_elements.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_flex.scss'),
-			this.destinationPath(scssDestination + 'scss/_flex.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_input.scss'),
-			this.destinationPath(scssDestination + 'scss/_input.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_menu.scss'),
-			this.destinationPath(scssDestination + 'scss/_menu.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_misc.scss'),
-			this.destinationPath(scssDestination + 'scss/_misc.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_parent.scss'),
-			this.destinationPath(scssDestination + 'scss/_parent.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_placeholder.scss'),
-			this.destinationPath(scssDestination + 'scss/_placeholder.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_string.scss'),
-			this.destinationPath(scssDestination + 'scss/_string.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_typography.scss'),
-			this.destinationPath(scssDestination + 'scss/_typography.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_wildcard.scss'),
-			this.destinationPath(scssDestination + 'scss/_wildcard.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/zurb/_foundation.scss'),
-			this.destinationPath(scssDestination + 'scss/zurb/_foundation.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/zurb/_global.scss'),
-			this.destinationPath(scssDestination + 'scss/zurb/_global.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/zurb/_settings.scss'),
-			this.destinationPath(scssDestination + 'scss/zurb/_settings.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_animation.scss'),
-			this.destinationPath(scssDestination + 'scss/mixins/_animation.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_background.scss'),
-			this.destinationPath(scssDestination + 'scss/mixins/_background.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_breakpoints.scss'),
-			this.destinationPath(scssDestination + 'scss/mixins/_breakpoints.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_calc.scss'),
-			this.destinationPath(scssDestination + 'scss/mixins/_calc.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_elements.scss'),
-			this.destinationPath(scssDestination + 'scss/mixins/_elements.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_flex.scss'),
-			this.destinationPath(scssDestination + 'scss/mixins/_flex.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_input.scss'),
-			this.destinationPath(scssDestination + 'scss/mixins/_input.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_menu.scss'),
-			this.destinationPath(scssDestination + 'scss/mixins/_menu.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_misc.scss'),
-			this.destinationPath(scssDestination + 'scss/mixins/_misc.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_parent.scss'),
-			this.destinationPath(scssDestination + 'scss/mixins/_parent.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_placeholder.scss'),
-			this.destinationPath(scssDestination + 'scss/mixins/_placeholder.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_string.scss'),
-			this.destinationPath(scssDestination + 'scss/mixins/_string.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_typography.scss'),
-			this.destinationPath(scssDestination + 'scss/mixins/_typography.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/mixins/_wildcard.scss'),
-			this.destinationPath(scssDestination + 'scss/mixins/_wildcard.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/_base.scss'),
-			this.destinationPath(scssDestination + 'scss/_base.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/_colors.scss'),
-			this.destinationPath(scssDestination + 'scss/_colors.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/_functions.scss'),
-			this.destinationPath(scssDestination + 'scss/_functions.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/_grid.scss'),
-			this.destinationPath(scssDestination + 'scss/_grid.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/_mixins.scss'),
-			this.destinationPath(scssDestination + 'scss/_mixins.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/_slick.scss'),
-			this.destinationPath(scssDestination + 'scss/_slick.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/_typography.scss'),
-			this.destinationPath(scssDestination + 'scss/_typography.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/_family.scss'),
-			this.destinationPath(scssDestination + 'scss/_family.scss')
-		);
-		this.fs.copy(
-			this.templatePath('scss/app.scss'),
-			this.destinationPath(scssDestination + 'scss/app.scss')
+			this.templatePath('scss/*'),
+			this.destinationPath(scssDestination + 'scss')
 		);
 
 		// ============= Common project files ==============
