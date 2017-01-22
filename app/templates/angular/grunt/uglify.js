b185178ddcf4d69f12fac5888c8be1f4f5f6816f
Stephan Hoogland <stephan@shoogland.com>
2017-01-22 17:04:12 +0100

    The vendor doesnt exist anymore

--- app/templates/angular/grunt/uglify.js
+++ app/templates/angular/grunt/uglify.js
@@ -1,8 +1,7 @@
 module.exports = {
 	demo: {
 		files: [{
-			'<%= config.dist.root %><?= appName ?>.js': ['<%= config.dist.root %><?= appName ?>.js'],
-			'<%= config.dist.root %>vendor.js': ['<%= config.dist.root %>vendor.js']
+			'<%= config.dist.root %><?= appName ?>.js': ['<%= config.dist.root %><?= appName ?>.js']
 		}]
 	}
 };
