f8ed517e365a28924c27baf13d8d2ae25bf3894d
silvandiepen <me@silvandiepen.com>
2017-08-29 17:13:09 +0200

    the big update

--- app/templates/wordpress/theme/page.php
+++ app/templates/wordpress/theme/page.php
@@ -8,3 +8,10 @@
  * @version      1.0
  * @since        1.0
  */
+ 
+ ?>
+<section id="home">
+	<h1>Hoi</h1>
+  <p>Welkom bij dit project, je staat nu aan de beginnen om iets heel erg moois te creeeren!</p>
+  <p>Veel plezier, geniet er van en kusjes.. </p>
+</section>
