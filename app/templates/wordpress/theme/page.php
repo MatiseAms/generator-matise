965dce4e37053df3c184335985455dd838f4606a
silvandiepen <me@silvandiepen.com>
2017-08-30 10:35:16 +0200

    new startup page

--- app/templates/wordpress/theme/page.php
+++ app/templates/wordpress/theme/page.php
@@ -10,8 +10,40 @@
  */
  
  ?>
-<section id="home">
-	<h1>Hoi</h1>
-  <p>Welkom bij dit project, je staat nu aan de beginnen om iets heel erg moois te creeeren!</p>
-  <p>Veel plezier, geniet er van en kusjes.. </p>
-</section>
+<header id="header">
+	<h4>Header</h4>
+	<nav id="nav" class="nav">
+	  <ul class="nav__list">
+	    <li class="nav__list--item">
+	      <span class="nav__list--item-text">
+	        <a href="" class="nav__list--item-link">Home</a>
+	      </span>
+	    </li>
+	    <li class="nav__list--item">
+	      <span class="nav__list--item-text">
+	        <a href="" class="nav__list--item-link">Work</a>
+	      </span>
+	    </li>
+	    <li class="nav__list--item">
+	      <span class="nav__list--item-text">
+	        <a href="" class="nav__list--item-link">Contact</a>
+	      </span>
+	    </li>
+	  </ul>
+	</nav>
+</header>
+
+<main>
+	<section id="home">
+		<h1>Hoi</h1>
+		<p hipsum paragraphs="2"></p>
+		<p>Welkom bij dit project, je staat nu aan de beginnen om iets heel erg moois te creëren!</p>
+		<p>Veel plezier, geniet er van en kusjes.. </p>
+	  <hr />
+	  <p>Need help? <a class="button-purple" href="http://docs.matise.design">Matise Docs</a></p>
+	</section>
+</main>
+
+<footer>
+	<h4>Footer</h4>
+</footer>
