44a7a076fc86aa70df8dbfa2b4b37808f8a52624
Stephan Hoogland <stephan@shoogland.com>
2018-02-20 11:12:44 +0100

    added style to the correct place in the project

--- app/templates/wordpress/theme/functions.php
+++ app/templates/wordpress/theme/functions.php
@@ -13,6 +13,13 @@
  * Enqueue scripts and styles.
  */
 function theme_scripts() {
+	$include_url = get_template_directory_uri();
+	$include_url = str_replace('https://', '//', $include_url);
+	$include_url = str_replace('http://', '//', $include_url);
+	
+	// Loads our main stylesheet.
+	wp_enqueue_style('theme-css', $include_url.'/css/app.css');
+	
 	// Theme stylesheet.
 	wp_enqueue_style( 'theme-style', get_stylesheet_uri() );
 
